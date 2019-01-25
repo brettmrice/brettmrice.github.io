3e1a433c6dc9e950968bad77d6e6e26c78e6130d
brettmrice <brettmrice@gmail.com>
2019-01-24 22:42:48 -0500

    draw galaxy ripple

--- default.js
+++ default.js
@@ -4,62 +4,56 @@ mode_2 = 0;
 count = 0;
 s_old = 61;
 
-//seed = 0;
 x_pos = [];
 y_pos = [];
 gal_width = [];
 num_gal_rings = [];
-gal_num = 5;
+gal_count = 0;
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   max_x_img = windowWidth;
   max_y_img = windowHeight;
-  mil = millis();
-<<<<<<< HEAD
-  //background(0);
-=======
+  mil = millis(); // for draw_galaxy_ripples
+  noStroke();
+  noFill();
   background(0);
->>>>>>> e354980ee51ac63f13aa91e5e1e48834969ccc39
+  gal_num = round(max(windowWidth, windowHeight)*.2);
 }
 
 function draw() {
+  print(windowWidth, windowHeight);
   if(value == 1) {
     draw_clock();
   } else {
-    //clear();
-<<<<<<< HEAD
-    //draw_galaxy_ripples();
-=======
+    if (gal_count == gal_num) {
+      noLoop();
+    }
     draw_galaxy_ripples();
->>>>>>> e354980ee51ac63f13aa91e5e1e48834969ccc39
   }
 }
 
 function draw_galaxy_ripples() {
-  if(mil/1000 > 1) {
-    background(0, 1);
-    for(i = 0; i < gal_num; i++) {
-      gal_width[i] = random(windowWidth*.1, windowWidth*.3);
-      x_pos[i] = round(random(gal_width[i]*1.1/2,
-        max_x_img-(gal_width[i]*1.1/2)));
-      y_pos[i] = round(random(gal_width[i]*1.1/2,
-        max_y_img-(gal_width[i]*1.1/2)));
-      num_gal_rings[i] = round(random(2, 4));
-    }
-    for(i = 0; i < gal_num; i++) {
-      for(j = 0; j < gal_width[i]; j++) {
-        ring_opac = sin(pow(PI, num_gal_rings[i])/(j));
-        stroke(255, (randomGaussian()*100) * ring_opac);
-        ellipse(x_pos[i], y_pos[i], j, j);
-      }
-    }
+  gal_width = random(4, 60);
+  x_pos = round(random(-gal_width*.9,
+    max_x_img+(gal_width*.9)));
+  y_pos = round(random(-gal_width*.9,
+    max_y_img+(gal_width*.9)));
+  num_gal_rings = round(random(2, 4));
+  for(j = 0; j < gal_width; j++) {
+    ring_opac = sin(pow(PI, num_gal_rings)/(j));
+    stroke(255, (randomGaussian()*100) * ring_opac);
+    ellipse(x_pos, y_pos, j, j);
   }
+  fill(0, 200, 0);
+  ellipse(x_pos, y_pos, gal_width*.05, gal_width*.05);
+  noFill();
+  gal_count++;
 }
 
 function draw_clock() {
   stroke(mode_2);
-  //if(s != s_old) {
   fill(mode_2);
   noStroke();
   ellipse(windowWidth/2, windowHeight/2,
@@ -73,10 +67,6 @@ function draw_clock() {
 
   fill(mode_1);
   ellipse(windowWidth/2, windowHeight/2, 20, 20);
-  //}
-  //s_old = s;
-  //m_old = m;
-<<<<<<< HEAD
 }
 
 function draw_clock_background() {
@@ -103,9 +93,6 @@ function draw_clock_background() {
   ellipse(windowWidth/2, windowHeight/2,
     min(windowWidth, windowHeight)*(2/2.1),
     min(windowWidth, windowHeight)*(2/2.1));
-    //count += 1;
-=======
->>>>>>> e354980ee51ac63f13aa91e5e1e48834969ccc39
 }
 
 function draw_second() {
@@ -145,92 +132,46 @@ function draw_hour() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
-  translate(windowWidth/2, windowHeight/2);
-  stroke(mode_1);
-  fill(mode_1);
-  background(mode_2);
-  for(i = 0; i < 60; i++) {
-    if(i != 0) {
-      rotate((TWO_PI/60)+PI);
-    }
-    if(i%5 == 0) {
-      strokeWeight(18);
-      rect(0, 0, 0, max(windowWidth, windowHeight));
-    } else {
-      strokeWeight(2);
-      rect(0, 0, 0, max(windowWidth, windowHeight));
-    }
+  if (value == 1) {
+    draw_clock_background();
+  } else {
+    max_x_img = windowWidth;
+    max_y_img = windowHeight;
+    background(0);
+    gal_count = 0;
+    loop();
+    draw_galaxy_ripples();
   }
-  s_old = 61;
-  translate(-windowWidth/2, -windowHeight/2);
-  fill(mode_2);
-  noStroke();
-  ellipse(windowWidth/2, windowHeight/2,
-    min(windowWidth, windowHeight)*(2/2.1),
-    min(windowWidth, windowHeight)*(2/2.1));
 }
 
 function mouseClicked() {
   if(mouseButton == LEFT) {
     if(count == 0) { // draw dark clock
       value = 1; // draw clock
-<<<<<<< HEAD
       count += 1;
       mode_1 = 255;
       mode_2 = 0;
       draw_clock_background()
+      loop();
     } else if(count == 1) { // draw bright clock
       value = 1; // draw clock
-=======
->>>>>>> e354980ee51ac63f13aa91e5e1e48834969ccc39
       count += 1;
-    } else if(count == 1) { // draw bright clock
-      value = 1; // draw clock
-      //count += 1;
       mode_1 = 0;
       mode_2 = 255;
-<<<<<<< HEAD
       draw_clock_background()
-=======
->>>>>>> e354980ee51ac63f13aa91e5e1e48834969ccc39
+      loop();
     } else if(count == 2) { // clear all drawings
       value = 0;
-      //count = 0;
+      count = 0;
       mode_1 = 255;
       mode_2 = 0;
-<<<<<<< HEAD
-      clear();
-      //background(0);
-    }
-=======
-      background(0);
-    }
-    if(count < 2) {
-      translate(windowWidth/2, windowHeight/2);
-      stroke(mode_1);
-      fill(mode_1);
-      background(mode_2);
-      for(i = 0; i < 60; i++) {
-        if(i != 0) {
-          rotate((TWO_PI/60)+PI);
-        }
-        if(i%5 == 0) {
-          strokeWeight(18);
-          rect(0, 0, 0, max(windowWidth, windowHeight));
-        } else {
-          strokeWeight(2);
-          rect(0, 0, 0, max(windowWidth, windowHeight));
-        }
-      }
-      s_old = 61;
-      translate(-windowWidth/2, -windowHeight/2);
-      fill(mode_2);
+      strokeWeight(1);
       noStroke();
-      ellipse(windowWidth/2, windowHeight/2,
-        min(windowWidth, windowHeight)*(2/2.1),
-        min(windowWidth, windowHeight)*(2/2.1));
-      count += 1;
-      }
->>>>>>> e354980ee51ac63f13aa91e5e1e48834969ccc39
+      noFill();
+      clear();
+      background(0);
+      gal_count = 0;
+      loop();
+    }
   }
 }
