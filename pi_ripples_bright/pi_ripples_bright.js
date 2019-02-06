bb5ad7be692985ff41e31dee64402db42084ed78
brettmrice <brettmrice@gmail.com>
2019-02-06 18:40:39 -0500

    update tab names

---
+++ pi_ripples_bright/pi_ripples_bright.js
@@ -0,0 +1,57 @@
+function preload() {
+  result = loadStrings('pi');
+}
+
+mode_1 = 0;
+mode_2 = 255;
+str_weight = [];
+str = [];
+obs = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  noFill();
+  pi_res = int(split(result[0], ''));
+  pi_res = subset(pi_res, 2, 1000002);
+  ellipse_max_size = dist(0, 0, windowWidth, windowHeight);
+  ellipse_count = 0;
+  pi_digit = 0;
+  num_pix_sp = 10;
+  subset_length = round(ellipse_max_size/num_pix_sp);
+  for(i = 0; i <= 9; i++) {
+    str_weight[i] = map(i, 0, 9, 10, 30);
+    str[i] = map(i, 0, 9, mode_1, mode_2);
+  }
+  obs_size = 20;
+  for(j = obs_size; j >= 1; j--) {
+    obs[j] = round(map(j, obs_size, 1, 0, 255));
+  }
+}
+
+function draw() {
+  drawPItarget();
+}
+
+function drawPItarget() {
+  pi_res_current = subset(pi_res, pi_digit, subset_length);
+  for(i = 0; i < subset_length; i++) {
+    strokeWeight(str_weight[pi_res_current[i]]);
+    stroke(str[pi_res_current[i]]);
+    ellipse(windowWidth/2, windowHeight/2,
+      ellipse_max_size - round(i*num_pix_sp),
+      ellipse_max_size - round(i*num_pix_sp));
+  }
+  for(j = obs_size; j >= 1; j--) {
+    strokeWeight(2);
+    stroke(mode_2, obs[j]);
+    ellipse(windowWidth/2, windowHeight/2, j, j);
+  }
+  pi_digit++;
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  ellipse_max_size = dist(0, 0, windowWidth, windowHeight);
+  subset_length = round(ellipse_max_size/num_pix_sp);
+
+}
--- pi_ripples_dark/pi_ripples_dark.js
+++ pi_ripples_dark/pi_ripples_dark.js
@@ -4,6 +4,9 @@ function preload() {
 
 mode_1 = 255;
 mode_2 = 0;
+str_weight = [];
+str = [];
+obs = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -14,7 +17,15 @@ function setup() {
   ellipse_count = 0;
   pi_digit = 0;
   num_pix_sp = 10;
-  subset_length = round(ellipse_max_size/round((num_pix_sp/2)+1));
+  subset_length = round(ellipse_max_size/num_pix_sp);
+  for(i = 0; i <= 9; i++) {
+    str_weight[i] = map(i, 0, 9, 10, 30);
+    str[i] = map(i, 0, 9, mode_1, mode_2);
+  }
+  obs_size = 20;
+  for(j = obs_size; j >= 1; j--) {
+    obs[j] = round(map(j, obs_size, 1, 0, 255));
+  }
 }
 
 function draw() {
@@ -24,15 +35,15 @@ function draw() {
 function drawPItarget() {
   pi_res_current = subset(pi_res, pi_digit, subset_length);
   for(i = 0; i < subset_length; i++) {
-    strokeWeight(map(pi_res_current[i], 0, 9, 10, 30));
-    stroke(map(pi_res_current[i], 0, 9, mode_1, mode_2));
+    strokeWeight(str_weight[pi_res_current[i]]);
+    stroke(str[pi_res_current[i]]);
     ellipse(windowWidth/2, windowHeight/2,
-      ellipse_max_size - (i*round((num_pix_sp/2)+1)),
-      ellipse_max_size - (i*round((num_pix_sp/2)+1)));
+      ellipse_max_size - round(i*num_pix_sp),
+      ellipse_max_size - round(i*num_pix_sp));
   }
-  for(j = 30; j >= 1; j--){
-    strokeWeight(1);
-    stroke(mode_1, map(j, 30, 1, 0, 255));
+  for(j = obs_size; j >= 1; j--) {
+    strokeWeight(2);
+    stroke(mode_2, obs[j]);
     ellipse(windowWidth/2, windowHeight/2, j, j);
   }
   pi_digit++;
@@ -41,5 +52,6 @@ function drawPItarget() {
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   ellipse_max_size = dist(0, 0, windowWidth, windowHeight);
-  subset_length = round(ellipse_max_size/round((num_pix_sp/2)+1));
+  subset_length = round(ellipse_max_size/num_pix_sp);
+
 }
